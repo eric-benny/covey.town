a1e262fcf08a26cb8edaa9ab5e481f4bcb81a2e7
jaqpet <jaqpet123@gmail.com>
2021-04-08 13:18:23 -0400

    Deleted Alvin's comment

--- frontend/src/components/world/CoveySubMapScene.ts
+++ frontend/src/components/world/CoveySubMapScene.ts
@@ -24,26 +24,3 @@ export default class CoveySubMapScene extends CoveySuperMapScene {
   }
 
 }
-
-  /*
-  Scenario: SuperMap player steps on to tile trigger player is now in subMap
-  function needed to: validate that the player has entered the subMap tile
-
-  Scenario: SuperMap player is now in the subMap, they want to leave, so they need to
-  step onto the exit tile, which is the exit trigger
-  function needed to: an exit trigger to set the map.id of hte player to == 0.
-  then send this information back to the backend
-
-  Scenario : SubMap player wants to exit the game from the subMap
-
-  Scenario: we are at capacity for the map e.g. 100/100 players.  The 100th player
-  is in the subMap.  If at capacity, is the player still allowed to enter new subMaps?
-
-  Scenario: we are at capacity for the map e.g. 100/100 players.  The 100th player
-  is in the subMap.  They want to exit the game from the subMap. Do we want to notify
-  the SuperMap or an event listener that we are no longer at capacity?
-
-  Scenario: SuperMap player needs a validation that they can video with
-  another subMap player.
-  function needed to: set the property that they can video each other
-   */
